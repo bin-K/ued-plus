1a013f7980ac14cfa98ef4293730879204d0fda6
kuangzhibin <1520257238@qq.com>
2024-04-11 17:31:31 +0800

    feat(scrollbar): 完成scrollbar always 属性配置开发
    affects: @ued-plus/components
    
    ISSUES CLOSED: none

--- packages/components/src/scrollbar/scrollbar.vue
+++ packages/components/src/scrollbar/scrollbar.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="ued-scrollbar">
 		<div
+			ref="wrapRef"
 			class="ued-scrollbar__wrap"
 			:class="scrollbarWrapClass"
 			:style="scrollbarWrapStyle"
@@ -8,13 +9,28 @@
 			<component :is="scrollbarTag" class="ued-scrollbar__view">
 				<slot />
 			</component>
+
 			<template v-if="!native">
-				<div class="ued-scrollbar__bar is-horizontal">
-					<div class="ued-scrollbar__thumb" style="width: 25px"></div>
-				</div>
-				<div class="ued-scrollbar__bar is-vertical">
-					<div class="ued-scrollbar__thumb" style="height: 129.6px"></div>
-				</div>
+				<transition name="ued-scrollbar-fade">
+					<div
+						v-show="always || visible"
+						ref="barHorizontalRef"
+						class="ued-scrollbar__bar is-horizontal"
+						:style="barHorizontalStyle"
+					>
+						<div class="ued-scrollbar__thumb" style="width: 25px"></div>
+					</div>
+				</transition>
+				<transition name="ued-scrollbar-fade">
+					<div
+						v-show="always || visible"
+						ref="barVerticalRef"
+						class="ued-scrollbar__bar is-vertical"
+						:style="barVerticalStyle"
+					>
+						<div class="ued-scrollbar__thumb" style="height: 129.6px"></div>
+					</div>
+				</transition>
 			</template>
 		</div>
 	</div>
@@ -22,14 +38,20 @@
 
 <script lang="ts" setup>
 import './styles/index.scss'
-import { computed } from 'vue'
+import { computed, ref, nextTick, onBeforeUnmount, CSSProperties } from 'vue'
 
 defineOptions({ name: 'UedScrollbar' })
+
+type Overflow = {
+	horizontal: boolean
+	vertical: boolean
+}
 
 type ScrollBarProps = {
 	tag?: string
 	height?: string | number
 	native?: boolean
+	always?: boolean
 }
 
 const scrollBarProps = defineProps<ScrollBarProps>()
@@ -52,5 +74,64 @@ const scrollbarWrapClass = computed(() => {
 
 const scrollbarTag = computed(() => {
 	return scrollBarProps.tag ?? 'div'
+})
+
+const visible = ref(false)
+
+const judgeOverflow = (): Overflow => {
+	if (wrapRef.value) {
+		const { scrollWidth, clientWidth, scrollHeight, clientHeight } =
+			wrapRef.value
+		return {
+			horizontal: scrollWidth > clientWidth,
+			vertical: scrollHeight > clientHeight,
+		}
+	}
+	return {
+		horizontal: false,
+		vertical: false,
+	}
+}
+
+const barHorizontalStyle = ref<CSSProperties>({
+	display: 'none',
+})
+
+const barVerticalStyle = ref<CSSProperties>({
+	display: 'none',
+})
+
+const mouseenter = () => {
+	if (!scrollBarProps.always) {
+		visible.value = true
+		judgeOverflow().horizontal && (barHorizontalStyle.value.display = undefined)
+		judgeOverflow().vertical && (barVerticalStyle.value.display = undefined)
+	}
+}
+
+const mouseleave = () => {
+	if (!scrollBarProps.always) {
+		visible.value = false
+		barHorizontalStyle.value.display = 'none'
+		barVerticalStyle.value.display = 'none'
+	}
+}
+
+const wrapRef = ref<HTMLDivElement>()
+const barHorizontalRef = ref<HTMLDivElement>()
+const barVerticalRef = ref<HTMLDivElement>()
+
+nextTick(() => {
+	barHorizontalStyle.value.display =
+		scrollBarProps.always && judgeOverflow().horizontal ? undefined : 'none'
+	barVerticalStyle.value.display =
+		scrollBarProps.always && judgeOverflow().vertical ? undefined : 'none'
+	wrapRef.value?.addEventListener('mouseenter', mouseenter)
+	wrapRef.value?.addEventListener('mouseleave', mouseleave)
+})
+
+onBeforeUnmount(() => {
+	wrapRef.value?.removeEventListener('mouseenter', mouseenter)
+	wrapRef.value?.removeEventListener('mouseleave', mouseleave)
 })
 </script>

ce02a14c65ec38a266e0b3cc4af6dec8df065a9a
kuangzhibin <1520257238@qq.com>
2024-04-13 11:50:17 +0800

    feat(scrollbar): scrollbar thumb 高度宽度计算
    affects: @ued-plus/components
    
    ISSUES CLOSED: none

--- packages/components/src/scrollbar/scrollbar.vue
+++ packages/components/src/scrollbar/scrollbar.vue
@@ -5,9 +5,10 @@
 			class="ued-scrollbar__wrap"
 			:class="scrollbarWrapClass"
 			:style="scrollbarWrapStyle"
+			@scroll="handleScroll"
 		>
 			<component
-				:is="scrollbarTag"
+				:is="tag"
 				class="ued-scrollbar__view"
 				:class="scrollbarViewClass"
 				:style="scrollbarViewStyle"
@@ -23,7 +24,10 @@
 						class="ued-scrollbar__bar is-horizontal"
 						:style="barHorizontalStyle"
 					>
-						<div class="ued-scrollbar__thumb" style="width: 25px"></div>
+						<div
+							class="ued-scrollbar__thumb"
+							:style="barHorizontalThumbStyle"
+						></div>
 					</div>
 				</transition>
 				<transition name="ued-scrollbar-fade">
@@ -33,7 +37,10 @@
 						class="ued-scrollbar__bar is-vertical"
 						:style="barVerticalStyle"
 					>
-						<div class="ued-scrollbar__thumb" style="height: 129.6px"></div>
+						<div
+							class="ued-scrollbar__thumb"
+							:style="barVerticalThumbStyle"
+						></div>
 					</div>
 				</transition>
 			</template>
@@ -43,28 +50,72 @@
 
 <script lang="ts" setup>
 import './styles/index.scss'
-import { computed, ref, nextTick, onBeforeUnmount, CSSProperties } from 'vue'
-
-defineOptions({ name: 'UedScrollbar' })
+import {
+	computed,
+	ref,
+	watch,
+	CSSProperties,
+	PropType,
+	onBeforeUnmount,
+	onMounted,
+	onUpdated,
+	nextTick,
+} from 'vue'
 
 type Overflow = {
 	horizontal: boolean
 	vertical: boolean
 }
 
-type ScrollBarProps = {
-	tag?: string
-	height?: string | number
-	maxHeight?: string | number
-	native?: boolean
-	always?: boolean
-	wrapStyle?: CSSProperties | CSSProperties[] | string[]
-	wrapClass?: string
-	viewStyle?: CSSProperties | CSSProperties[] | string[]
-	viewClass?: string
-}
+defineOptions({ name: 'UedScrollbar' })
 
-const scrollBarProps = defineProps<ScrollBarProps>()
+const scrollBarProps = defineProps({
+	tag: {
+		type: String,
+		default: 'div',
+	},
+	height: {
+		type: [String, Number],
+		default: undefined,
+	},
+	maxHeight: {
+		type: [String, Number],
+		default: undefined,
+	},
+	native: {
+		type: Boolean,
+		default: false,
+	},
+	always: {
+		type: Boolean,
+		default: false,
+	},
+	wrapStyle: {
+		type: [Array, Object] as PropType<
+			CSSProperties | CSSProperties[] | string[]
+		>,
+		default: undefined,
+	},
+	wrapClass: {
+		type: String,
+		default: undefined,
+	},
+	viewStyle: {
+		type: [Array, Object] as PropType<
+			CSSProperties | CSSProperties[] | string[]
+		>,
+		default: undefined,
+	},
+	viewClass: {
+		type: String,
+		default: undefined,
+	},
+	minSize: {
+		type: Number,
+		default: 20,
+	},
+})
+const emits = defineEmits(['scroll'])
 
 const scrollbarWrapStyle = computed(() => {
 	const height =
@@ -85,15 +136,12 @@ const scrollbarWrapStyle = computed(() => {
 })
 
 const scrollbarWrapClass = computed(() => {
-	const wrapClass = scrollBarProps.wrapClass
-		? {
-				[scrollBarProps.wrapClass]: true,
-			}
-		: {}
-	return {
-		'ued-scrollbar__wrap--hidden-default': !scrollBarProps.native,
-		...wrapClass,
-	}
+	return [
+		scrollBarProps.wrapClass,
+		{
+			'ued-scrollbar__wrap--hidden-default': !scrollBarProps.native,
+		},
+	]
 })
 
 const scrollbarViewStyle = computed(() => {
@@ -103,22 +151,86 @@ const scrollbarViewStyle = computed(() => {
 })
 
 const scrollbarViewClass = computed(() => {
-	const viewClass = scrollBarProps.viewClass
-		? {
-				[scrollBarProps.viewClass]: true,
-			}
-		: {}
+	return [scrollBarProps.viewClass]
+})
+
+const wrapRef = ref<HTMLDivElement>()
+
+const GAP = 4
+const moveX = ref(0)
+const moveY = ref(0)
+const ratioX = ref(1)
+const ratioY = ref(1)
+const sizeWidth = ref('')
+const sizeHeight = ref('')
+
+const barHorizontalRef = ref<HTMLDivElement>()
+const barVerticalRef = ref<HTMLDivElement>()
+const barHorizontalStyle = ref<CSSProperties>({
+	display: 'none',
+})
+
+const barVerticalStyle = ref<CSSProperties>({
+	display: 'none',
+})
+
+const barHorizontalThumbStyle = computed<CSSProperties>(() => {
 	return {
-		...viewClass,
+		width: sizeWidth.value,
+		transform: `translateX(${moveX.value})`,
+	}
+})
+const barVerticalThumbStyle = computed<CSSProperties>(() => {
+	return {
+		height: sizeHeight.value,
+		transform: `translateY(${moveY.value})`,
 	}
 })
 
-const scrollbarTag = computed(() => {
-	return scrollBarProps.tag ?? 'div'
-})
+const handleScroll = () => {
+	if (wrapRef.value) {
+		const { offsetHeight, offsetWidth, scrollTop, scrollLeft } = wrapRef.value
+		moveY.value = ((scrollTop * 100) / (offsetHeight - GAP)) * ratioY.value
+		moveX.value = ((scrollLeft * 100) / (offsetWidth - GAP)) * ratioX.value
+		emits('scroll', {
+			scrollTop: wrapRef.value.scrollTop,
+			scrollLeft: wrapRef.value.scrollLeft,
+		})
+	}
+}
+
+const update = () => {
+	if (!wrapRef.value) return
+	const { offsetHeight, offsetWidth, scrollHeight, scrollWidth } = wrapRef.value
+	const originalHeight = (offsetHeight - GAP) ** 2 / scrollHeight
+	const originalWidth = (offsetWidth - GAP) ** 2 / scrollWidth
+	const height = Math.max(originalHeight, scrollBarProps.minSize)
+	const width = Math.max(originalWidth, scrollBarProps.minSize)
+
+	ratioY.value =
+		originalHeight /
+		(offsetHeight - originalHeight) /
+		(height / (offsetHeight - height))
+	ratioX.value =
+		originalWidth /
+		(offsetWidth - originalWidth) /
+		(width / (offsetWidth - width))
+
+	sizeHeight.value = height + GAP < offsetHeight ? `${height}px` : ''
+	sizeWidth.value = width + GAP < offsetWidth ? `${width}px` : ''
+}
+
+watch(
+	() => [scrollBarProps.height, scrollBarProps.maxHeight],
+	() => {
+		if (!scrollBarProps.native) {
+			update()
+			handleScroll()
+		}
+	}
+)
 
 const visible = ref(false)
-
 const judgeOverflow = (): Overflow => {
 	if (wrapRef.value) {
 		const { scrollWidth, clientWidth, scrollHeight, clientHeight } =
@@ -133,15 +245,6 @@ const judgeOverflow = (): Overflow => {
 		vertical: false,
 	}
 }
-
-const barHorizontalStyle = ref<CSSProperties>({
-	display: 'none',
-})
-
-const barVerticalStyle = ref<CSSProperties>({
-	display: 'none',
-})
-
 const mouseenter = () => {
 	if (!scrollBarProps.always) {
 		visible.value = true
@@ -149,7 +252,6 @@ const mouseenter = () => {
 		judgeOverflow().vertical && (barVerticalStyle.value.display = undefined)
 	}
 }
-
 const mouseleave = () => {
 	if (!scrollBarProps.always) {
 		visible.value = false
@@ -158,21 +260,27 @@ const mouseleave = () => {
 	}
 }
 
-const wrapRef = ref<HTMLDivElement>()
-const barHorizontalRef = ref<HTMLDivElement>()
-const barVerticalRef = ref<HTMLDivElement>()
-
-nextTick(() => {
-	barHorizontalStyle.value.display =
-		scrollBarProps.always && judgeOverflow().horizontal ? undefined : 'none'
-	barVerticalStyle.value.display =
-		scrollBarProps.always && judgeOverflow().vertical ? undefined : 'none'
-	wrapRef.value?.addEventListener('mouseenter', mouseenter)
-	wrapRef.value?.addEventListener('mouseleave', mouseleave)
+onMounted(() => {
+	if (!scrollBarProps.native) {
+		nextTick(() => {
+			update()
+			barHorizontalStyle.value.display =
+				scrollBarProps.always && judgeOverflow().horizontal ? undefined : 'none'
+			barVerticalStyle.value.display =
+				scrollBarProps.always && judgeOverflow().vertical ? undefined : 'none'
+			wrapRef.value?.addEventListener('mouseenter', mouseenter)
+			wrapRef.value?.addEventListener('mouseleave', mouseleave)
+		})
+	}
 })
-
+onUpdated(() => update())
 onBeforeUnmount(() => {
 	wrapRef.value?.removeEventListener('mouseenter', mouseenter)
 	wrapRef.value?.removeEventListener('mouseleave', mouseleave)
+})
+
+defineExpose({
+	handleScroll,
+	update,
 })
 </script>

db26e78c6e60f42c4b89da5f9552b1d1468b2db2
Kuangzhibin <1520257238@qq.com>
2024-04-29 00:15:59 +0800

    refactor(mono): 调整类型文件
    affects: @ued-plus/components
    
    ISSUES CLOSED: none

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,4 +1,5 @@
-import Insatller from './default'
+import { Plugin, App } from 'vue'
+import components from './components'
 // 引入全局样式变量
 import '../theme/common/var.scss'
 // 引入初始化css文件
@@ -6,5 +7,14 @@ import '../theme/initialize/init.scss'
 
 export * from './src/index'
 
-export const install = Insatller.install
-export default Insatller
+export default ((components: Plugin[] = []) => {
+	const install = (app: App) => {
+		for (const c in components) {
+			app.use(components[c])
+		}
+	}
+
+	return {
+		install,
+	}
+})(components)

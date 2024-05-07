0873cc381998fe408026b9f00a8e2c293bc0781a
kuangzhibin <1520257238@qq.com>
2024-05-07 14:53:22 +0800

    refactor(icon): 调整Icon组件引入，避免重复引入(changelog-needed)
    affects: @ued-plus/components
    
    ISSUES CLOSED: none

--- packages/components/src/icon/components/common/index.vue
+++ packages/components/src/icon/components/common/index.vue
@@ -1,16 +1,12 @@
 <template>
-	<component :is="iconSvg" :name="name" />
+	<component :is="Icons[name as keyof typeof Icons]" :name="name" />
 </template>
 <script lang="ts" setup>
-import { defineAsyncComponent } from 'vue'
-
-const iconSvg = defineAsyncComponent(
-	() => import(`../${iconProps.name}/index.vue`)
-)
+import Icons from '../../icon'
 
 type IconProps = {
 	name: string
 }
 
-const iconProps = defineProps<IconProps>()
+defineProps<IconProps>()
 </script>
